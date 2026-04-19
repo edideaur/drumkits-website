cf0d905b158ff9ded3dc8c18b599dd85cff99686
edideaur <eduard@prigoana.com>
2026-04-19 19:13:11 +0300

    fix workflow

--- scraper.py
+++ scraper.py
@@ -20,6 +20,7 @@ SESSION.headers.update({
     "cache-control": "no-cache",
     "pragma": "no-cache",
     "priority": "u=1, i",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36",
     "sec-ch-ua": '"Chromium";v="146", "Not-A.Brand";v="24", "Google Chrome";v="146"',
     "sec-ch-ua-mobile": "?0",
     "sec-ch-ua-platform": '"Windows"',
